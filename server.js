b98d6f30fe4c850b117715310775c40d6d994b95
Clark <rclark010@gmail.com>
2019-03-20 14:32:42 -0600

    UppercaseName middleware complete.

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ const server = express();
 
 // middleware
 server.use(express.json());
+// uppercase middleware
+function uppercaseName(req, res, next) {
+  if (req.body.hasOwnProperty('name')) {
+    req.body.name = req.body.name.toUpperCase();
+  }
+  next();
+}
+server.use(uppercaseName);
 
 // routing
 server.use('/api/posts', postsRoutes);
